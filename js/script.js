8a0fad030f870ed1f5f91a73fd3e1035b3933b10
Antonius131 <antomendetta@gmail.com>
2022-06-16 14:56:18 +0200

    "for" cycle to print numbers (1 to 100) in console

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// devo scrivere un programma che stampi numeri da 1 a 100
+// il programma deve stampare la parola "Fizz" quando i = multiplo di 3 (% 3 = 0)
+// il programma deve stampare la parola "Buzz" quando i = multiplo di 5 (% 5 = 0)
+// il programma deve stampare la parola "FizzBuzz" quando i = multiplo di 3 e 5 [(% 3 = 0)  &&  (% 5 = 0)]
+
+for (let i = 1; i <= 100; i++) {
+   console.log(i);
+}
