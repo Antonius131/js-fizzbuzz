04c267406d3565af66bda3c56d925043ebb9f361
Antonius131 <antomendetta@gmail.com>
2022-06-16 17:56:06 +0200

    append boxWrapper to fizzbuzzContainer

--- js/script.js
+++ js/script.js
@@ -2,24 +2,27 @@
 // il programma deve stampare la parola "Fizz" quando i = multiplo di 3 (% 3 = 0)
 // il programma deve stampare la parola "Buzz" quando i = multiplo di 5 (% 5 = 0)
 // il programma deve stampare la parola "FizzBuzz" quando i = multiplo di 3 e 5 [(% 3 = 0)  &&  (% 5 = 0)]
+const fizzbuzzContainer = document.getElementById('fizz-buzz-wrapper');
 
 for (let i = 1; i <= 100; i++) {
 
    // create a div
    const boxWrapper = document.createElement('div');
    // add col-2 class to boxWrapper
-   boxWrapper.classList.add('col-2');
+   boxWrapper.classList.add('col-3');
+   // append boxWrapper in the row #fizz-buzz-wrapper
+   fizzbuzzContainer.append(boxWrapper);
 
    let fizz = i % 3 == 0;
    let buzz = i % 5 == 0;
    let fizzBuzz = ((fizz) && (buzz));
 
    if (fizzBuzz) {
-      console.log("FizzBuzz");
+      console.log('FizzBuzz');
    } else if (fizz) {
-      console.log("Fizz");
+      console.log('Fizz');
    } else if (buzz) {
-      console.log("Buzz");
+      console.log('Buzz');
    } else {
       console.log(i);
    }
