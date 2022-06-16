a1a855d8c0595f0759c9c791d3c4d6f2daf44930
Antonius131 <antomendetta@gmail.com>
2022-06-16 16:24:24 +0200

    milestone 1: strings printed in console

--- js/script.js
+++ js/script.js
@@ -4,5 +4,19 @@
 // il programma deve stampare la parola "FizzBuzz" quando i = multiplo di 3 e 5 [(% 3 = 0)  &&  (% 5 = 0)]
 
 for (let i = 1; i <= 100; i++) {
-   console.log(i);
+
+   let fizz = i % 3 == 0;
+   let buzz = i % 5 == 0;
+   let fizzBuzz = ((fizz == true) && (buzz == true));
+
+   if ((fizz == true) && (buzz == false)) {
+      console.log("Fizz");
+   } else if ((buzz == true) && (fizz == false)) {
+      console.log("Buzz");
+   } else if (fizzBuzz == true) {
+      console.log("FizzBuzz");
+   } else {
+      console.log(i);
+   }
+   
 }
